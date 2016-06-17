d66fccdd653cc9f9825d1e5eb4b116f631005407
batrakov_ag <leetim@live.ru>
2016-06-17 23:47:50 +1000

    Решил переписать змейку заново, немного изменю саму игру. Залил код для точек, он весьма не плох, только придется изменить код для рандомного места на карте.

---
+++ include/Point.h
@@ -0,0 +1,31 @@
+#pragma once
+
+struct Point{
+	typedef Point& reference;
+	Point(): x(), y(){};
+	Point(int _x, int _y): x(_x), y(_y){};
+	Point(const Point& other);
+	reference operator=(const Point& other);
+	Point operator-();
+	int len2();
+
+	static Point randomPoint(int max_x, int max_y);
+	static Point dirByCode(int code);
+	static const Point dir_top;
+	static const Point dir_right;
+	static const Point dir_bottom;
+	static const Point dir_left;
+
+	friend Point operator+(const Point& left, const Point& right);
+	friend Point operator-(const Point& left, const Point& right);
+	friend bool operator==(const Point& left, const Point& right);
+	friend bool operator!=(const Point& left, const Point& right);
+	//Семантика данных функций не совсем очивидна, смотрите код object.cpp
+	//На экране если right лежит в правом нижнем секторе относительно left, то
+	//возвращает тру
+	friend bool operator<(const Point& left, const Point& right);
+	friend bool operator>(const Point& left, const Point& right);
+
+	int x;
+	int y;
+};
---
+++ src/Point.cpp
@@ -0,0 +1,87 @@
+#include <Point.h>
+#include <stdlib.h>
+#include <cmath>
+#include <iostream>
+#define MAAAX 1000000
+
+using namespace std;
+
+const Point Point::dir_top = Point(0, -1);
+const Point Point::dir_left = Point(-1, 0);
+const Point Point::dir_bottom = Point(0, 1);
+const Point Point::dir_right = Point(1, 0);
+
+Point::Point(const Point& other){
+	x = other.x;
+	y = other.y;
+}
+
+Point Point::randomPoint(int max_x, int max_y){
+	double v1 = 2*((double)(rand()%MAAAX))/MAAAX - 1;
+	double v2 = 2*((double)(rand()%MAAAX))/	MAAAX - 1;
+	double s = sqrt(v1*v1 + v2*v2);
+	if (s > 1){
+		return randomPoint(max_x, max_y);
+	}
+	double u1 = v1*sqrt(-2*log(s)/s);
+	double u2 = v2*sqrt(-2*log(s)/s);
+	u1 = u1;
+	u2 = u2;
+	int x = ceil(max_x*u1);
+	int y = ceil(max_y*u2);
+	return Point(x, y);
+}
+
+Point Point::dirByCode(int code){
+	switch(code){
+	case 0:
+		return dir_left;
+	case 1:
+		return dir_top;
+	case 2:
+		return dir_right;
+	case 3:
+		return dir_bottom;
+	default:
+		return Point();
+	}
+}
+
+Point& Point::operator=(const Point& other){
+	x = other.x;
+	y = other.y;
+	return *this;
+}
+
+int Point::len2(){
+	return x*x + y*y;
+}
+
+Point Point::operator-(){
+	return Point(-x, -y);
+}
+
+Point operator+(const Point& l, const Point& r){
+	return Point(l.x + r.x, l.y + r.y);
+}
+
+
+Point operator-(const Point& l, const Point& r){
+	return Point(l.x - r.x, l.y - r.y);
+}
+
+bool operator==(const Point& l, const Point& r){
+	return (l.x == r.x)&&(l.y == r.y);
+}
+
+bool operator!=(const Point& l, const Point& r){
+	return !(l == r);
+}
+
+
+bool operator<(const Point& l, const Point& r){
+	return (l.x < r.x && l.y < r.y) && l != r;
+}
+bool operator>(const Point& l, const Point& r){
+	return (l.x > r.x && l.y > r.y) && l != r;
+}
